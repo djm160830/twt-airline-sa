9c6ca8ff4149bc702b6d9a34e4f3200c92c81ebc
djm160830 <djm160830@utdallas.edu>
2020-10-28 17:22:39 -0500

    Final commit before submission 2

--- hw4.py
+++ hw4.py
@@ -12,7 +12,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 import numpy as np
 from prettytable import PrettyTable
-import pdb
 
 
 # TODO 1 & 2
@@ -48,11 +47,11 @@ def preprocess(data):
 	return data, counts, enc
 
 if __name__ == "__main__":
-	t = PrettyTable(['ITERATION', 'ALPHA', 'ACCURACY'])
+	t = PrettyTable(['ITERATION', 'ALPHA', 'FIT_PRIOR', 'ACCURACY'])
 
 	# TODO 7:  Repeat this process 5 times with different parameter choices and output the parameters and accuracy in a tabular format.
-	for i, a in enumerate([0.0001, 0.2, 0.4, 0.6, 0.8]):
-		for z in range(5):
+	for i, a in enumerate(np.linspace(0.2, 1.25, 5)):
+		for _, fp in enumerate([False, False, False, True, True, True]):
 			"""
 			TODO 1: Read in tweets
 			TODO 2: Read in airline_sentiment, airline, and text into a dataframe
@@ -71,13 +70,13 @@ if __name__ == "__main__":
 			X_train, X_test, y_train, y_test = train_test_split(counts, y_encoded, test_size=0.1)
 
 			# TODO 5: Build a Multinomial Naïve Bayes (MNB) model using the training dataset. You have to choose the best set of parameters.
-			model = MultinomialNB(alpha=a).fit(X_train, y_train)
+			model = MultinomialNB(alpha=a, fit_prior=fp).fit(X_train, y_train)
 
 			# TODO 6: Apply your model on test and output the accuracy
 			predicted = model.predict(X_test)
 			accuracy = model.score(X_test, y_test)
-			t.add_row([i+1, a, accuracy])
-		if i!=4: t.add_row([' ', ' ', ' '])	
+			t.add_row([i+1, a, fp, accuracy])
+		if i!=4: t.add_row([' ', ' ', ' ', ' '])	
 	print(t)
 
 	"""
