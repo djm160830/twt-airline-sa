273eb7950b053e46949cc0efe222019cefcfa83d
Darla M <38709959+djm160830@users.noreply.github.com>
2021-02-09 15:35:58 -0600

    Clarify code documentation

--- twt-airline-sa.py
+++ twt-airline-sa.py
@@ -10,13 +10,30 @@ import numpy as np
 from prettytable import PrettyTable
  
 
-# TODO 1 & 2
 def read_tweets():
+	"""Reads Tweets from US Airline Sentiment dataset.
+
+	Returns:
+	DataFrame: Table containing Tweets, which airline the Tweet is referring to, and sentiment of Tweet
+
+	"""
 	return pd.read_csv("https://raw.githubusercontent.com/djm160830/twt-airline-sa/master/archive/Tweets.csv", 
 		usecols=["airline_sentiment", "airline", "text"])
 
-# TODO 3
 def preprocess(data):
+	"""Converts words to lowercase, performs label encoding, train/test split, tf-idf scores
+	
+	Parameters: 
+	data (DataFrame): Table containing Tweets, which airline the Tweet is referring to, and sentiment of Tweet
+	
+	Returns:
+	counts (sparse matrix): Transformed count vectorized matrix of words from training dataset into a tf-idf representation
+	X_test_tf (sparse matrix): Transformed count vectorized matrix of words from test dataset into a tf-idf representation
+	y_train (Series): Label encoded target variable from training dataset
+	y_test (Series): Label encoded target variable from testing dataset
+	data['airline_sentiment'] (DataFrame): Table of label encoded sentiments
+	X_test (DataFrame): Raw feature variables
+	"""
 	# Convert text to lowercase 
 	for column in data.columns:
 		data[column] = data[column].str.lower()
@@ -25,7 +42,7 @@ def preprocess(data):
 	le = preprocessing.LabelEncoder()
 	data['airline_sentiment'] = le.fit_transform(data['airline_sentiment'])
 
-	# TODO 4: Split data into training and testing (10% testing) using train_test_split
+	# Split data into training and testing (10% testing) using train_test_split
 	X_train, X_test, y_train, y_test = train_test_split(data.iloc[:, 1:], data.iloc[:, 0], test_size=0.10)
  
 	# Transform training text using countvectorizer and tfidftransformer
@@ -42,43 +59,31 @@ def preprocess(data):
 	X_test_cv = count_vect.transform(X_test['text']) 
 	X_test_tf = transformer.transform(X_test_cv)
 
-	# return data, counts, enc
 	return counts, X_test_tf, y_train, y_test, data['airline_sentiment'], X_test
 
 if __name__ == "__main__":
 	t = PrettyTable(['ITERATION', 'ALPHA', 'FIT_PRIOR', 'TRAINING ACCURACY'])
 
-	# TODO 7:  Repeat process 5 times with different parameter choices (for laplace smoothing & fit_prior) and output the parameters and accuracy in a tabular format.
+	# Repeat process 5 times with different parameter choices a, fp (for laplace smoothing & fit_prior) and output the parameters and accuracy in a tabular format.
 	for i, a in enumerate(np.linspace(1.25, 1.0e-10, 5)):
 	  for _, fp in enumerate([True, True, True, False, False, False]):
-	    """
-	    TODO 1: Read in tweets
-	    TODO 2: Read in airline_sentiment, airline, and text into a dataframe
-	    """
+	    # Read Tweets
 	    df = read_tweets()
-
-	    """
-	    TODO 3: Text preprocessing steps: 
-	    - convert text to lowercase
-	    - transform the training text using countvectorizer and tfidftransformer,
-	    - convert airline_sentiment from categorical to numerical values using labelencoder
-	    """
+		
 	    X_train, X_test, y_train, y_test, target_sentiment, X_test_raw = preprocess(df)
 
-	    # TODO 5: Build a Multinomial Naïve Bayes (MNB) model using the training dataset
+	    # Build Multinomial Naïve Bayes (MNB) model 
 	    model = MultinomialNB(alpha=a, fit_prior=fp).fit(X_train, y_train)
 
-	    # TODO 6: Apply model on test and output the accuracy
+	    # Apply model on test and output accuracy
 	    predicted = model.predict(X_test) 
 	    accuracy = model.score(X_train, y_train)
 	    t.add_row([i+1, a, fp, accuracy])
 	  if i!=4: t.add_row([' ', ' ', ' ', ' '])	
 	print(t)
 
-	"""
-	TODO 8: Outputting the average sentiment of each airline and
+	# Output avg sentiment of each airline, and report airline with highest sentiment
 	reporting which airline has the highest positive sentiment.
-	""" 
 	df['airline_sentiment'] = target_sentiment
 	highest_sentiment = df.groupby('airline').agg(mean_sentiment=('airline_sentiment', 'mean')).sort_values(by='mean_sentiment', ascending=False)
 	print(f'\n{highest_sentiment}')
